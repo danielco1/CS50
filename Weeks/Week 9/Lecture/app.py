a60cddc7cf0d19fbe236d409ea07f787af200ee8
danielco1 <danielcommit@zohomail.eu>
2025-11-30 14:07:36 +0100

    Update

--- Weeks/Week 9/Lecture/app.py
+++ Weeks/Week 9/Lecture/app.py
@@ -2,15 +2,14 @@ from flask import Flask, render_template, request  # type: ignore
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
 
-@app.route("/greet", methods=["POST"])
-def greet():
-    # Use request.form for POST data
-    name = request.form.get("name", "Guest")
-    return render_template("greet.html", name=name)
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        return render_template("greet.html", name=request.form.get("name"))
+    else:
+        return render_template("index.html")
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
