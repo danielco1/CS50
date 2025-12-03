705b1bdf4fa1dc95ba9556f68b31deec5184c04a
danielco1 <danielcommit@zohomail.eu>
2025-12-03 20:47:47 +0100

    Update

---
+++ Weeks/Week 9/Lecture/login/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, session
+from flask import Session
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    if request.method == "POST":
+        session["name"] = request.form.get("name")
+        return redirect("/")
+    return render_template("login.html")
