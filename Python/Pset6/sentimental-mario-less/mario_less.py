b660f7a2ca722bb87a862cfd176ac6db3aed11cf
Daniel <danielcommit@zohomail.eu>
2025-12-05 16:44:45 +0100

    Delete Python/Pset6 directory

--- Python/Pset6/sentimental-mario-less/mario_less.py
+++
@@ -1,8 +0,0 @@
-while True:
-    height = input("Height: ")
-    if height.isdigit():
-        height = int(height)
-        if 1 <= height <= 8:
-            break
-for i in range(1, height + 1):
-    print(" " * (height - i) + "#" * i)
