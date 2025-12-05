5b96190d7c3adf00ac1f403c93f6088977506b62
danielco1 <danielcommit@zohomail.eu>
2025-12-05 16:33:00 +0100

    Update

--- Weeks/Week 9/Lecture/login/app.py
+++ Weeks/Week 9/Lecture/login/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, redirect, request, session
 from flask import Session
 
 app = Flask(__name__)
@@ -9,11 +9,17 @@ Session(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", name=session.get("name"))
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         session["name"] = request.form.get("name")
         return redirect("/")
     return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+    
